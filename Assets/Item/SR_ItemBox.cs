799ad1f498a6f3c17ed92a70fa827a225c81d34f
H.Ototsuji <nkjthat25@gmail.com>
2024-06-22 13:21:51 +0900

    Item, ItemBox作成

---
+++ Assets/Item/SR_ItemBox.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+
+public class SR_ItemBox : MonoBehaviour
+{
+    [SerializeField][Tooltip("スピードアップに必要なアイテム数")][Min(1)] int _speedUpThreshold = 3;
+
+    [SerializeField]int _itemCount;
+    [SerializeField]int _itemCountForSpeedUp;
+
+    private BoxCollider _boxCol;
+
+    void Start()
+    {
+        TryGetComponent<BoxCollider>(out _boxCol);
+        _boxCol.isTrigger = true;
+
+        _itemCount = 0;
+
+    }
+
+    private void Update()
+    {
+        SpeedUpProcess();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            ItemGetProcess();
+
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+
+    private void SpeedUpProcess()
+    {
+        if (_itemCountForSpeedUp >= _speedUpThreshold)
+        {
+            //速度アップ呼び出し
+            _itemCountForSpeedUp = 0;
+        }
+    }
+
+    private void ItemGetProcess()
+    {
+        /*アイテム取得数を増やす*/
+        _itemCount++;
+        _itemCountForSpeedUp++;
+
+        //スタミナ減少値Up呼び出し
+    }
+}
