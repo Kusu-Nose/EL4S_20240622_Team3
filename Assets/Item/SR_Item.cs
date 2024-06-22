47b4f33ed888f880ff8f29401dc82b9cc8b0aceb
DESKTOP-1PSHM9A\Administrator <atroriass@gmail.com>
2024-06-22 15:26:35 +0800

    メロン生成

--- Assets/Item/SR_Item.cs
+++ Assets/Item/SR_Item.cs
@@ -6,11 +6,23 @@ public class SR_Item : MonoBehaviour
 {
     [SerializeField][Tooltip("‰ñ“]‘¬“x")] float _rotVolume = 0.01f;
 
+    GameObject playerController;
+    private void Start()
+    {
+        playerController = GameObject.FindWithTag("Player");
+    }
     // Update is called once per frame
     void Update()
     {
         var rotation = transform.rotation;
         rotation *= Quaternion.AngleAxis(_rotVolume, Vector3.up);
         transform.rotation = rotation;
+
+
+        if(playerController.transform.position.z - transform.position.z > 10.0f)
+        {
+            Destroy(this.gameObject);
+        }
     }
+
 }
